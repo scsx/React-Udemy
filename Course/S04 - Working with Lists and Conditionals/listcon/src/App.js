63a736cc6f783e5614502caecac2c01642b92b5b
scsx <jsoucasaux@gmail.com>
2019-10-18 16:52:08 +0100

    Weekend commit :)

--- Course/S04 - Working with Lists and Conditionals/listcon/src/App.js
+++ Course/S04 - Working with Lists and Conditionals/listcon/src/App.js
@@ -12,44 +12,59 @@ class App extends Component {
             {name: 'Manu',age: 29},
             {name: 'Stephanie',age: 26}
         ],
-        otherState: 'some other value'
+        otherState: 'some other value',
+        showPersons: true
     }
 
-    switchNameHandler = (newName) => {
-        this.setState({
-            persons: [
-                {name: newName,age: 28},
-                {name: 'Manu',age: 29},
-                {name: 'Stephanie',age: 27}
-            ]
-        })
+    deletePersonHandler = (personIndex) => {
+        // const currentPersons = this.state.persons; // this deletes from state, bad practice
+        const currentPersons = [...this.state.persons]; // this way we make a copy
+        currentPersons.splice(personIndex, 1);
+        this.setState({persons: currentPersons});
     }
 
     nameChangedHandler = (event) => {
         this.setState({
             persons: [
-                {name: 'Max',age: 28},
-                {name: event.target.value,age: 29},
-                {name: 'Stephanie',age: 26}
+                {id: '00', name: 'Max', age: 28},
+                {id: '01', name: event.target.value, age: 29},
+                {id: '02', name: 'Stephanie', age: 26}
             ]
         })
     }
 
+    togglePersonsHandler = () => {
+        const doesShow = this.state.showPersons;
+        // this merges with the current state
+        this.setState({showPersons: !doesShow});
+    }
+
     render() {
+
+        let personsHTML = null;
+        if (this.state.showPersons) {
+            personsHTML = (
+                <div className="people">
+
+                    {this.state.persons.map((guy, index) => {
+                        return <Person key={guy.id} delClick={() => this.deletePersonHandler(index)} name={guy.name} age={guy.age} />
+                    })}
+
+                    {/* <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+                    <Person name={this.state.persons[1].name} age={this.state.persons[1].age} click={this.switchNameHandler.bind(
+                        this, 'Max!' )} changed={this.nameChangedHandler} />
+                    <Person name={this.state.persons[2].name} age={this.state.persons[2].age} /> */}
+                </div>
+            );
+        }
 
         return (
             <div className="App">
                 <h1>App.js</h1>
+                <button className="btn btn-success mt-4" onClick={this.togglePersonsHandler}>Toggle Persons</button>
 
-                <div>
-                    <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-                    <Person name={this.state.persons[1].name} age={this.state.persons[1].age} click={this.switchNameHandler.bind(
-                        this, 'Max!' )} changed={this.nameChangedHandler} />
-                    <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+                {personsHTML}
 
-                    <button className="btn btn-success mt-4" onClick={this.togglePersonsHandler}>Toggle Persons</button>
-
-                </div>
             </div>
         );
     }
