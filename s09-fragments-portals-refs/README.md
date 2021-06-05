205b106a2712a59e1589d4a5ecd87b15c032c2d2
scsx <4182349+scsx@users.noreply.github.com>
2021-06-05 10:32:09 +0100

    End section 9

--- s09-fragments-portals-refs/README.md
+++ s09-fragments-portals-refs/README.md
@@ -1,1 +1,3 @@
 # Section 9: Diving Deeper: Working with Fragments, Portals & "Refs"
+- Example of a simple form without state management at: src\components\Users\AddUser.js
+https://github.com/academind/react-complete-guide-code/tree/09-fragments-portals-refs
--- s09-fragments-portals-refs/src/components/Users/AddUser.js
+++ s09-fragments-portals-refs/src/components/Users/AddUser.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 
 import Card from '../UI/Card'
 import Button from '../UI/Button'
@@ -7,14 +7,17 @@ import Wrapper from '../helpers/Wrapper'
 import classes from './AddUser.module.css'
 
 const AddUser = (props) => {
-    const [enteredUsername, setEnteredUsername] = useState('')
-    const [enteredAge, setEnteredAge] = useState('')
+    const nameInputRef = useRef()
+    const ageInputRef = useRef()
     const [error, setError] = useState()
 
     const addUserHandler = (event) => {
+        const enteredName = nameInputRef.current.value
+        const enteredAge = ageInputRef.current.value
+
         event.preventDefault()
         if (
-            enteredUsername.trim().length === 0 ||
+            enteredName.trim().length === 0 ||
             enteredAge.trim().length === 0
         ) {
             setError({
@@ -30,17 +33,9 @@ const AddUser = (props) => {
             })
             return
         }
-        props.onAddUser(enteredUsername, enteredAge)
-        setEnteredUsername('')
-        setEnteredAge('')
-    }
-
-    const usernameChangeHandler = (event) => {
-        setEnteredUsername(event.target.value)
-    }
-
-    const ageChangeHandler = (event) => {
-        setEnteredAge(event.target.value)
+        props.onAddUser(enteredName, enteredAge)
+        nameInputRef.current.value = '' // This should not be done but it's porssible
+        ageInputRef.current.value = ''
     }
 
     const errorHandler = () => {
@@ -62,15 +57,13 @@ const AddUser = (props) => {
                     <input
                         id='username'
                         type='text'
-                        value={enteredUsername}
-                        onChange={usernameChangeHandler}
+                        ref={nameInputRef}
                     />
                     <label htmlFor='age'>Age (Years)</label>
                     <input
                         id='age'
                         type='number'
-                        value={enteredAge}
-                        onChange={ageChangeHandler}
+                        ref={ageInputRef}
                     />
                     <div className='mt-3'>
                         <Button type='submit'>Add User</Button>
