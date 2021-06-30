908e7db96e63e5c29cc50c99250c4c3f22c70a6a
scsx <4182349+scsx@users.noreply.github.com>
2021-06-30 23:22:33 +0100

    s11 start with scss and ant design

---
+++ s11-course-project--food-order/src/App.js
@@ -0,0 +1,14 @@
+import './scss/App.scss'
+import { Button } from 'antd'
+
+function App() {
+    return (
+        <div className='App'>
+            <header className='App-header'>
+                <Button type='primary'>Button</Button>
+            </header>
+        </div>
+    )
+}
+
+export default App
