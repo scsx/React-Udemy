65dc295fb1a8eb460b54964aa462926e5522620d
scsx <4182349+scsx@users.noreply.github.com>
2021-07-28 03:42:04 +0100

    Input working before refactoring

--- s16-Forms/src/App.js
+++ s16-Forms/src/App.js
@@ -2,7 +2,8 @@ import SimpleInput from './components/SimpleInput'
 
 function App() {
     return (
-        <div className='container app py-5'>
+        <div className='container app py-4'>
+            <h1>MyTravel</h1>
             <SimpleInput />
         </div>
     )
--- s16-Forms/src/components/SimpleInput.js
+++ s16-Forms/src/components/SimpleInput.js
@@ -1,12 +1,46 @@
+import { useState } from 'react'
+
 const SimpleInput = (props) => {
+    const [enteredName, setEnteredName] = useState('')
+    const [enteredNameTouched, setEnteredNameTouched] = useState(false)
+
+    const enteredNameIsValid = enteredName.trim() !== ''
+    const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched
+
+    const nameInputChangeHandler = (event) => {
+        setEnteredName(event.target.value)
+    }
+
+    const nameInputBlurHandler = () => {
+        setEnteredNameTouched(true)
+    }
+
+    const formSubmissionHandler = (event) => {
+        event.preventDefault()
+        setEnteredNameTouched(true)
+        if (!enteredNameIsValid) {
+            return
+        }
+        console.log('Form submitted with name ' + enteredName)
+        setEnteredName('')
+        setEnteredNameTouched(false)
+    }
+
+    const nameInputClasses = nameInputIsInvalid
+        ? 'form-control is-invalid'
+        : 'form-control'
+
     return (
-        <form>
+        <form onSubmit={formSubmissionHandler}>
             <div className='input-group input-group-lg mb-3'>
                 <input
                     type='text'
-                    className='form-control'
-                    placeholder="Recipient's username"
+                    className={nameInputClasses}
+                    placeholder='name'
                     id='name'
+                    onChange={nameInputChangeHandler}
+                    onBlur={nameInputBlurHandler}
+                    value={enteredName}
                 />
                 <button
                     className='btn btn-primary'
@@ -15,6 +49,9 @@ const SimpleInput = (props) => {
                     Submit
                 </button>
             </div>
+            {nameInputIsInvalid && (
+                <p className='text-white'>Name must not be empty</p>
+            )}
         </form>
     )
 }
